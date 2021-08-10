75e822cfc3011957fa53674fc3f4693bb3cbbe39
Nikola Vuchev <nikola_vuchev@abv.bg>
2021-08-10 15:03:52 +0300

    LongestCommonPrefix

---
+++ LongestCommonPrefix.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class LongestCommonPrefix {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        String[] words = scan.nextLine().split(" ");
+
+        System.out.println(longestCommonPrefix(words));
+    }
+
+    public static String longestCommonPrefix(String[] strs) {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < strs[0].length(); i++) {
+            char c = strs[0].charAt(i);
+            for (int j = 1; j < strs.length; j++)
+                if (i == strs[j].length() || strs[j].charAt(i) != c) return sb.toString();
+            sb.append(c);
+        }
+        return sb.toString();
+
+    }
+}
+
