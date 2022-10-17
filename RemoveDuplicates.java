bd7b0152c3d9ec0b5ef237aabea442b23d23fdf9
Nikola Vuchev <nikola_vuchev@abv.bg>
2022-10-17 18:38:16 +0300

    RemoveDuplicates(Easy)

--- RemoveDuplicates.java
+++ RemoveDuplicates.java
@@ -4,19 +4,25 @@ public class RemoveDuplicates {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        int [] numbers = Arrays.stream(scan.nextLine().split(" "))
+        int[] numbers = Arrays.stream(scan.nextLine().split(","))
                 .mapToInt(Integer::parseInt).toArray();
 
         System.out.println(removeDuplicates(numbers));
+        Arrays.stream(numbers).forEach(e -> System.out.print(e + " "));
     }
 
     public static int removeDuplicates(int[] nums) {
-        Set<Integer> duplicates = new TreeSet<>();
-
-        for(int i = 0; i < nums.length; i++){
-                duplicates.add(nums[i]);
+        int index = 1;
+        int k = 1;
+        for (int i = 0; i < nums.length - 1; i++) {
+            int first = nums[i];
+            int second = nums[i + 1];
+            if (first != second) {
+                nums[index] = second;
+                index++;
+                k++;
+            }
         }
-
-        return duplicates.size();
+        return k;
     }
 }
